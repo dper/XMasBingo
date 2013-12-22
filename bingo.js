97d8b9fa6464801da218c72105b611f1af55f0ce
Douglas P. Perkins <contact@dperkins.org>
2013-12-22 18:17:45 +0900

    Images put in array.

--- bingo.js
+++ bingo.js
@@ -1,6 +1,16 @@
 var files = new Array();
 files=["Bear.png", "Bells.png", "Bow.png", "Candle.png", "Cane.png", "Card.png", "Carolers.png", "Coal.png", "Doll.png", "Fireplace.png", "Holly.png", "Horse.png", "Lights.png", "Ornament.png", "Present.png", "Reindeer.png", "Santa.png", "Sleigh.png", "Snowflake.png", "Snowman.png", "Soldier.png", "Star.png", "Stocking.png", "Toys.png", "Train.png", "Tree.png", "Wreath.png"];
 
-document.write("<tr><td>");
-document.write(files[0]);
-document.write("</td></tr>");
+var rows = 4;
+var columns = 4;
+
+for (var row = 0; row < rows; row++) {
+	document.write("<tr>");
+	for (var column = 0; column < columns; column++) {
+		index = (4 * row) + column;
+		file = files[index];
+		img = "<img src=\"pictures/" + file + "\" />";
+		document.write("<td>" + img + "</td>");
+	}
+	document.write("</tr>")
+}
