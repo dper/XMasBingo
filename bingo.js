3b3954c534c355b2f16e4d55ec5b4046252a04a7
Douglas P. Perkins <contact@dperkins.org>
2013-12-22 18:08:05 +0900

    Adding basic JS.

---
+++ bingo.js
@@ -0,0 +1,6 @@
+var files = new Array();
+files=["Bear.png", "Bells.png", "Bow.png", "Candle.png", "Cane.png", "Card.png", "Carolers.png", "Coal.png", "Doll.png", "Fireplace.png", "Holly.png", "Horse.png", "Lights.png", "Ornament.png", "Present.png", "Reindeer.png", "Santa.png", "Sleigh.png", "Snowflake.png", "Snowman.png", "Soldier.png", "Star.png", "Stocking.png", "Toys.png", "Train.png", "Tree.png", "Wreath.png"];
+
+document.write("<tr><td>");
+document.write(files[0]);
+document.write("</td></tr>");
