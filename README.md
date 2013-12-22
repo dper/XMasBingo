a8f17c9fb3f5f9107a803c4a2d0eaca565015170
Douglas Perkins <webmaster@dperkins.org>
2013-12-22 22:40:49 +0900

    Adding randomize.

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 XMasBingo
 =========
 
-A Christmas Bingo sheet maker.
+A Christmas Bingo sheet maker.  There are twenty seven pictures, though a given board only uses sixteen.  The images are chosen randomly.  Refresh the page to get a random new board.
+
+The `html` file has style information in the top of it.  If you find that the boards are not the right size for your needs, you can make a copy of the project and then tweak the CSS until you get what you need.
+
+The underlying code can easily be adapted to non-Christmas use.  The code here is very short and simple and, therefore, easy to modify to fit your needs.
 
 
 Sources
 =======
 
 * Most of the images are from Clker.  http://www.clker.com/
+* The array randomizer was written by Laurens Holst.  http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
--- bingo.js
+++ bingo.js
@@ -1,6 +1,23 @@
+/**
+ * Randomize array element order in-place using Fisher-Yates.
+ * Taken from a StackOverflow answer written by Laurens Holst.
+ * http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+ */
+function shuffleArray(array) {
+	for (var i = array.length - 1; i > 0; i--) {
+		var j = Math.floor(Math.random() * (i + 1));
+		var temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+	
+	return array;
+}
+
 var files = new Array();
 files=["Bear.png", "Bells.png", "Bow.png", "Candle.png", "Cane.png", "Card.png", "Carolers.png", "Coal.png", "Doll.png", "Fireplace.png", "Holly.png", "Horse.png", "Lights.png", "Ornament.png", "Present.png", "Reindeer.png", "Santa.png", "Sleigh.png", "Snowflake.png", "Snowman.png", "Soldier.png", "Star.png", "Stocking.png", "Toys.png", "Train.png", "Tree.png", "Wreath.png"];
 
+shuffleArray(files);
 var rows = 4;
 var columns = 4;
 
